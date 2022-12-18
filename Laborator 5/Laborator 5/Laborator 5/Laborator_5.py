82d70dec679c548a4a76c8971a5aeaf97ec794f9
Mihai Alexandru Iancu <97533213+mihaiiancu21@users.noreply.github.com>
2022-12-18 11:28:31 +0200

    Laborator 5

---
+++ Laborator 5/Laborator 5/Laborator 5/Laborator_5.py
@@ -0,0 +1,117 @@
+import pyautogui
+import keyboard
+import time
+
+#def cautare_google():
+#    if pyautogui.locateOnScreen(r'D:\Facultate\An 3\Inteligenta artificiala\04_11\searchbar.png', confidence=0.7) != None:
+#        camp_google = pyautogui.locateOnScreen(r'D:\Facultate\An 3\Inteligenta artificiala\04_11\searchbar.png', confidence=0.7)
+#        pyautogui.click(camp_google)
+#        time.sleep(3)
+#        pyautogui.write("https://youtube.com")
+#        pyautogui.press('enter')
+#        print("IMAGINEA SE AFLA PE ECRAN")
+#    else:
+#        print("IMAGINEA NU SE AFLA PE ECRAN")
+
+
+
+#def cautare_youtube():
+#    if pyautogui.locateOnScreen(r'D:\Facultate\An 3\Inteligenta artificiala\04_11\searchbarYT.png', confidence=0.7) != None:
+#        camp_youtube = pyautogui.locateOnScreen(r'D:\Facultate\An 3\Inteligenta artificiala\04_11\searchbarYT.png', confidence=0.7)
+#        pyautogui.click(camp_youtube)
+#        time.sleep(3)
+#        pyautogui.write("Freecodecamp")
+#        pyautogui.press('enter')
+#        print("IMAGINEA SE AFLA PE ECRAN")
+#    else:
+#        print("IMAGINEA NU SE AFLA PE ECRAN")
+
+#def apasare_nume():
+#    if pyautogui.locateOnScreen(r'D:\Facultate\An 3\Inteligenta artificiala\04_11\nume_canal.png', confidence=0.7) != None:
+#        nume_canal = pyautogui.locate']\OnScreen(r'D:\Facultate\An 3\Inteligenta artificiala\04_11\nume_canal.png', confidence=0.7)
+#        pyautogui.click(nume_canal)
+#        time.sleep(3)
+#        pyautogui.press('enter')
+#        print("IMAGINEA SE AFLA PE ECRAN")
+#    else:
+#        print("IMAGINEA NU SE AFLA PE ECRAN")
+
+#time.sleep(3)
+#cautare_google()
+#time.sleep(5)
+#cautare_youtube()
+#time.sleep(3)
+#apasare_nume()
+
+
+def coordonate():
+    print(pyautogui.position())
+
+time.sleep(4)
+coordonate()
+
+#time.sleep(1)
+#pyautogui.click(679,600)  #click pe yt
+
+#time.sleep(3)
+#pyautogui.click(497,93)  #click pe bara de search
+#time.sleep(2)
+#pyautogui.write('Freecodecamp')
+#pyautogui.press('enter')
+
+#time.sleep(3)
+#pyautogui.click(388,239)  #click pe canal
+
+#time.sleep(3)
+#pyautogui.click(405,389) #click pe videos
+
+#time.sleep(3)
+#pyautogui.click(363,553) #click pe video
+
+#time.sleep(10)
+#pyautogui.click(26,51) #click pe back
+
+#time.sleep(3)
+#pyautogui.click(627,624) #click pe video
+
+#time.sleep(10)
+#pyautogui.click(26,51) #click pe back
+
+#time.sleep(3)
+#pyautogui.click(894,666) #click pe video
+
+#time.sleep(10)
+#pyautogui.click(26,51) #click pe back
+
+#time.sleep(3)
+#pyautogui.click(1153,581) #click pe video
+
+#time.sleep(10)
+#pyautogui.click(26,51) #click pe back
+
+time.sleep(3)
+pyautogui.moveTo(1149,233)
+
+time.sleep(3)
+pyautogui.scroll(-250) #scroll
+
+time.sleep(3)
+pyautogui.click(366,578) #click pe video
+
+time.sleep(10)
+pyautogui.click(26,51) #click pe back'
+
+time.sleep(3)
+pyautogui.click(642,607) #click pe video
+
+time.sleep(10)
+pyautogui.click(26,51) #click pe back
+
+time.sleep(3)
+pyautogui.click(889,614) #click pe video
+
+time.sleep(10)
+pyautogui.click(26,51) #click pe back
+
+time.sleep(3)
+pyautogui.click(1143,606) #click pe video
